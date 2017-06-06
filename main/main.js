c3f0ddcbb6cf2017ea1915c5bc9c375204cefcea
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 13:14:14 +0200

    no message

--- main/main.js
+++ main/main.js
@@ -10,29 +10,23 @@ server.listen(8080); //start the webserver on port 8080
 app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
 
 
+var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
+sp.on("open", function () {
+    console.log('open');
+    var buf = new Buffer(1); //creates a new 1-byte buffer
+    buf.writeUInt8(1, 0); //writes the pwm value to the buffer
+    sp.write(buf);
+});
+sp.on('data', function(data) {
+    console.log('data received: ' + data);
+});
 
-server.listen(80, '127.0.0.5');
-
-
-try{
-    var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
-    sp.on("open", function () {
-        console.log('open');
+io.sockets.on('connection', function (socket){
+    socket.emit('test', { test: 'Its Working' });
+    socket.on('value', function (data){
+        console.log(data);
     });
-    sp.on('data', function(data) {
-        console.log('data received: ' + data);
-    });
-
-    io.sockets.on('connection', function (socket){
-        socket.emit('test', { test: 'Its Working' });
-        socket.on('value', function (data){
-            console.log(data);
-        });
-    });
-    
-} catch (error){
-    console.log(error);
-}
+});
 
 
 /*
