87f4a2240940c5c36ea22e359fa911018bd4c19c
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 11:52:56 +0200

    scoket

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')();
 const server = require('http').createServer(express);
 const io = require('socket.io').listen(server);
 const SerialPort = require("serialport").SerialPort;
