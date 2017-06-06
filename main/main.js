2b3b5f875f96d4a59ad059845327f4c0af7215f3
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 14:20:28 +0200

    no message

--- main/main.js
+++ main/main.js
@@ -7,27 +7,35 @@ let SerialPort = require("serialport");
 let dataReceived = "";
 
 app.get('/', (req, res) => {
+
     console.log('Request Params',req.params);
     console.log('Request Query',req.query);
-    res.send('Hello World!\n' + dataReceived)
+
+    var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
+    sp.on("open", () => {
+        console.log('open');
+        var buf = new Buffer(1); //creates a new 1-byte buffer
+        buf.writeUInt8(1, 0); //writes the pwm value to the buffer
+        sp.write(buf);
+    });
+    sp.on('data', (data) => {
+        dataReceived += "\n" + data;
+        console.log('data received: ' + data);
+    });
+    
+    res.send(
+        <div>
+            <p>Hello World</p>
+            <p>dataReceived</p>
+        </div>);
 });
 
-app.listen(8080, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(80, () => {
+    console.log('Example app listening on port 80!')
 });
 
 
-var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
-sp.on("open", () => {
-    console.log('open');
-    var buf = new Buffer(1); //creates a new 1-byte buffer
-    buf.writeUInt8(1, 0); //writes the pwm value to the buffer
-    sp.write(buf);
-});
-sp.on('data', (data) => {
-    dataReceived += "\n" + data;
-    console.log('data received: ' + data);
-});
+
 
 
 //SERVER
