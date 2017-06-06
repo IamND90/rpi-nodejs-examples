bf46e253abba035c77ddf5673f4933f5f500d4d1
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 14:40:55 +0200

    no message

--- main/main.js
+++ main/main.js
@@ -11,18 +11,6 @@ app.get('/', (req, res) => {
 
     console.log('Request Query',req.query);
 
-    var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
-    sp.on("open", () => {
-        console.log('open');
-        var buf = new Buffer(1); //creates a new 1-byte buffer
-        buf.writeUInt8(1, 0); //writes the pwm value to the buffer
-        sp.write(buf);
-    });
-    sp.on('data', (data) => {
-        dataReceived += "\n" + data;
-        console.log('data received: ' + data);
-    });
-
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -30,6 +18,14 @@ app.listen(80, () => {
     console.log('Example app listening on port 80!')
 });
 
-function showValue(value) {
-    console.log('Value',value);
-}
+var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
+sp.on("open", () => {
+    console.log('open');
+    var buf = new Buffer(1); //creates a new 1-byte buffer
+    buf.writeUInt8(1, 0); //writes the pwm value to the buffer
+    sp.write(buf);
+});
+sp.on('data', (data) => {
+    dataReceived += "\n" + data;
+    console.log('data received: ' + data);
+});
