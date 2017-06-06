b2ee797e31cd01768ee933abcb49a5da49a926d6
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 13:27:53 +0200

    no message

--- main/main.js
+++ main/main.js
@@ -2,12 +2,17 @@
 
 var express = require('express');  //web server
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);	//web socket server
 var SerialPort = require("serialport");
 
-server.listen(8080); //start the webserver on port 8080
-app.use(express.static('public')); //tell the server that ./public/ contains the static webpages
+var dataReceived = "";
+
+app.get('/', function (req, res) {
+    res.send('Hello World!\n' + dataReceived)
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
 
 
 var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
@@ -18,28 +23,10 @@ sp.on("open", function () {
     sp.write(buf);
 });
 sp.on('data', function(data) {
+    dataReceived += "\n" + data;
     console.log('data received: ' + data);
 });
 
-io.sockets.on('connection', function (socket){
-    socket.emit('test', { test: 'Its Working' });
-    socket.on('value', function (data){
-        console.log(data);
-    });
-});
-
-
-/*
-sp.open(portName, { // portName is instatiated to be COM3, replace as necessary
-    baudRate: 115200, // this is synced to what was set for the Arduino Code
-    dataBits: 8, // this is the default for Arduino serial communication
-    parity: 'none', // this is the default for Arduino serial communication
-    stopBits: 1, // this is the default for Arduino serial communication
-    flowControl: false // this is the default for Arduino serial communication
-}).catch((error) => {
-    console.log(error);
-});
-*/
 
 //SERVER
 
