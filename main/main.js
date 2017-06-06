e0fe5b58d21e789b39d05ab6b2e34d19e9678359
Andrey Paramonov <andrey.oj@gmail.com>
2017-06-06 13:46:14 +0200

    no message

--- main/main.js
+++ main/main.js
@@ -1,30 +1,30 @@
 "use-strict";
 
-var express = require('express');  //web server
+let express = require('express');  //web server
 let app = express();
-var SerialPort = require("serialport");
+let SerialPort = require("serialport");
 
-var dataReceived = "";
+let dataReceived = "";
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     console.log('Request Params',req.params);
     console.log('Request Query',req.query);
     res.send('Hello World!\n' + dataReceived)
 });
 
-app.listen(8080, function () {
+app.listen(8080, () => {
     console.log('Example app listening on port 3000!')
 });
 
 
 var sp = new SerialPort("/dev/ttyACM0", { baudrate: 115200 });
-sp.on("open", function () {
+sp.on("open", () => {
     console.log('open');
     var buf = new Buffer(1); //creates a new 1-byte buffer
     buf.writeUInt8(1, 0); //writes the pwm value to the buffer
     sp.write(buf);
 });
-sp.on('data', function(data) {
+sp.on('data', (data) => {
     dataReceived += "\n" + data;
     console.log('data received: ' + data);
 });
